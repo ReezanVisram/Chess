5a14f80b1f49990a0249173dc7979c4fa6d85552
Reezan Visram <rhvisram@uwaterloo.ca>
2022-12-17 10:57:53 -0500

    finish shaders

--- Chess.cpp
+++ Chess.cpp
@@ -64,6 +64,7 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		shader.use();
+		shader.setFloatUniform("offset", 0.0f);
 		glBindVertexArray(vao1);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
